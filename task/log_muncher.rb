fa818b5db87bd2440149e6234b8a6340b29b60aa
lopata <114728537+lekseyz@users.noreply.github.com>
2024-09-21 12:14:03 +0300

    dummy solution

--- task/log_muncher.rb
+++ task/log_muncher.rb
@@ -1,2 +1,25 @@
 include Gem::Text
-# levenshtein_distance('asd', 'sdf') # => 2
+
+result = {}
+count = 0
+
+File.open("A:/Prog_s/Ruby/ruby24_task1/data/log.txt", "r") do |file|
+  file.each_line do |line|
+    match = /=> ([А-Яа-яA-Za-z\sё]+)$/.match(line)
+
+    found = false
+    result.keys.each do |key|
+      if levenshtein_distance(key, match[1]) < 3
+        result[key] += 1
+        found = true
+        break
+      end
+    end
+
+    result[match[1]] = 1 unless found
+
+  end
+end
+
+print(result.count)
+
