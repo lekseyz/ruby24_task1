7568e6c6cb4a3507932cf0b0e6f7fec0db5aa7fd
lopata <114728537+lekseyz@users.noreply.github.com>
2024-09-25 22:03:32 +0300

    dummy solution

--- task/log_muncher.rb
+++ task/log_muncher.rb
@@ -1,25 +1,43 @@
 include Gem::Text
 
-result = {}
+def dist(str1, str2)
+  return false if (str1.length - str2.length).abs > 4
+  return true if levenshtein_distance(str1, str2) < 5
+  false
+end
+
+names = {}
 count = 0
 
+
+t1 = Time.now
 File.open("A:/Prog_s/Ruby/ruby24_task1/data/log.txt", "r") do |file|
   file.each_line do |line|
-    match = /=> ([А-Яа-яA-Za-z\sё]+)$/.match(line)
+    name = line[line.index('>')+1...line.length-1]
 
-    found = false
-    result.keys.each do |key|
-      if levenshtein_distance(key, match[1]) < 3
-        result[key] += 1
-        found = true
-        break
-      end
-    end
-
-    result[match[1]] = 1 unless found
+    names[name] = 0 if names[name].nil?
+    names[name] += 1
 
   end
 end
 
-print(result.count)
+wrongs = names.sort_by{|_,nums|-nums}.to_a.drop(200)
+puts("sorted")
+wrongs.each { |name, _| names.delete(name) }
+puts("deleted")
 
+wrongs.each do |name,_|
+  names.keys.each do |nameCor, _|
+    if dist(name, nameCor)
+      names[nameCor] += 1
+      next
+    end
+  end
+end
+
+names.each do |name,count|
+  puts "#{count} #{name}"
+end
+t2 = Time.now
+
+puts "Время работы программы: "+(t2-t1).to_s+" cек"
